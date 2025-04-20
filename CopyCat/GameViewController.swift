00ee898f0ff8314667907fa3124d70d426601304
Anaiq <Anaiq@example.com>
2025-04-20 10:36:34 -0500

    change play button on main/launchpad, work on replay button, timer layout change

--- CopyCat/GameViewController.swift
+++ CopyCat/GameViewController.swift
@@ -13,7 +13,11 @@ class GameViewController: UIViewController {
     
     @IBOutlet weak var replayIndex: UILabel!
     
-    var replays = 0
+    var currentNumberOfReplays = 0
+    var totalReplays = 3
+    var currentLevelIndex = 0
+    var totalLevels = 5
+    var cats: [UIImageView] = []
     
     
     override func viewDidLoad() {
@@ -22,15 +26,31 @@ class GameViewController: UIViewController {
         // Do any additional setup after loading the view.
         view.backgroundColor = .white // Or whatever your game background is
         self.navigationItem.hidesBackButton = true
+        replayIndex.text = "\(currentNumberOfReplays) of \(totalReplays) used."
     }
     
 
     @IBAction func didPushReplayButton(_ sender: UIButton) {
         print("Pressed replay!")
-        replays += 1
-        if (replays > 3) {
-//            alert(title: "You've ran out of replays")
+        currentNumberOfReplays += 1
+        if (currentNumberOfReplays > totalReplays) {
+            replayIndex.text = "\(currentNumberOfReplays - 1) of \(totalReplays) used."
+            showLastReplayAlert()
+        } else {
+            replayIndex.text = "\(currentNumberOfReplays) of \(totalReplays) used."
+        }
+        
+    }
+    
+    func showLastReplayAlert() {
+        let alertController = UIAlertController(title: "No More Replays", message: "You have no more replays!", preferredStyle: .alert)
+        let dismissAction = UIAlertAction(title: "Dismiss", style: .default) { _ in
             return
         }
+        alertController.addAction(dismissAction)
+        // Present the alert
+        present(alertController, animated: true, completion: nil)
     }
+    
+    
 }
